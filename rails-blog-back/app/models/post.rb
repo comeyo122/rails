b8b310fffc04bcc6e7a13894d61e143f1d968f00
hojin <hj2913@ajou.ac.kr>
2020-02-16 20:23:26 +0900

    remove back

--- rails-blog-back/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user, optional: true
-  has_many :likes, dependent: :nullify
-  has_many :comment, dependent: :nullify
-  mount_uploader :image, ImageUploader
-end
--- rails-blog-back/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, dependent: :nullify
-  has_many :likes, dependent: :nullify
-  has_many :comment, dependent: :nullify
-end
